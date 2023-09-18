7c0978cf34bde66aaacbaf741693749f1e081816
Zintis <zsusinskis@gmail.com>
2023-09-18 20:40:55 +0000

    added NASA API key

--- prac_no2/naked.py
+++ prac_no2/naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "eW2lamWLrciRFgUL8h6qJeMM8OytxMlyyoYOtTTQ"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
